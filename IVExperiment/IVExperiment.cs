0e0932ad0106eee319bd752d4956a69f43c905f3
zigorrom <zigorrom@gmail.com>
2015-02-17 16:53:29 +0100

    comm1

--- IVExperiment/IVExperiment.cs
+++ IVExperiment/IVExperiment.cs
@@ -7,9 +7,37 @@ using System.Threading.Tasks;
 
 namespace IVExperiment
 {
-    public class IVExperiment
+    public class IVExperiment:AbstractExperiment
     {
+        public IVExperiment():base("IV_MEASUREMENT")
+        {
+            InitializeInstruments();
+            InitializeExperiment();
+        }
 
-     
+        public override void InitializeExperiment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InitializeInstruments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ReportProgress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Abort()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
