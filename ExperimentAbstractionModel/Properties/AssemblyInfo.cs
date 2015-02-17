4e3b6267cf3324f555342d454e16415665078bbe
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-02-17 13:41:56 +0100

    New

--- ExperimentAbstractionModel/Properties/AssemblyInfo.cs
+++ ExperimentAbstractionModel/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c02e7aed-a515-4ab3-8565-0a632d8c1000")]
+[assembly: Guid("f98d0919-5923-4846-8fd6-a918a08625a6")]
 
 // Version information for an assembly consists of the following four values:
 //
