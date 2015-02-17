785291f1487b925f64bda83f2656a3fc0f2ade84
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-02-17 13:45:12 +0100

    Attached files

---
+++ InstrumentAbstractionModel/IInstrument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstrumentAbstractionModel
+{
+    public interface IInstrument
+    {
+       
+        string Name { get; set; }
+        string Alias { get; set; }
+        IInstrumentOwner InstrumentOwner { get; set; }
+        InstrumentState State { get; set; }
+        void DetectInstrument();
+    }
+}
---
+++ InstrumentAbstractionModel/IInstrumentOwner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InstrumentAbstractionModel
+{
+    public interface IInstrumentOwner
+    {
+        string Name { get; set; }
+    }
+}
