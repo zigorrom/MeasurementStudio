59d7582fb97bf4eb47801429db42090f1dc474fa
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-02-20 02:32:28 +0100

    InstrumentOwner equal method

--- InstrumentAbstractionModel/IInstrumentOwner.cs
+++ InstrumentAbstractionModel/IInstrumentOwner.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace InstrumentAbstractionModel
 {
+    
     public interface IInstrumentOwner:IEquatable<IInstrumentOwner>
     {
+        
         string Name { get; }
     }
 }
