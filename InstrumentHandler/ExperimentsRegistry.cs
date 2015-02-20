cb5083077a850b3c343ff6e9ffd5507d62783794
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-02-20 20:09:30 +0100

    ExperimentRegistry added

---
+++ InstrumentHandler/ExperimentsRegistry.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstrumentHandlerNamespace
+{
+    public sealed class ExperimentsRegistry
+    {
+        private static volatile ExperimentsRegistry m_experiments;
+        private static object syncRoot = new object();
+        private ExperimentsRegistry()
+        {
+
+        }
+        public static ExperimentsRegistry Instance
+        {
+            get
+            {
+                lock (syncRoot)
+                {
+                    if (m_experiments == null)
+                        m_experiments = new ExperimentsRegistry();
+                }
+                return m_experiments;
+            }
+        }
+    }
+}
