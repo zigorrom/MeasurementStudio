552f2517bb2ed2523c5149ba9a57dfbcf91af2f1
zigorrom <zigorrom@gmail.com>
2015-02-20 11:48:15 +0100

    Console.ReadKey

--- test/Program.cs
+++ test/Program.cs
@@ -14,6 +14,7 @@ namespace test
         {
             var handler = InstrumentHandlerNamespace.InstrumentHandler.Instance;
             handler.DiscoverInstruments();
+            Console.ReadKey();
         }
     
     }
