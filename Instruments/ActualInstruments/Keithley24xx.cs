0c975892518e8709162fca8aea86158e55df4fec
zigorrom <zigorrom@gmail.com>
2015-03-01 00:30:31 +0100

    devices writing

--- Instruments/ActualInstruments/Keithley24xx.cs
+++ Instruments/ActualInstruments/Keithley24xx.cs
@@ -7,14 +7,37 @@ using System.Threading.Tasks;
 namespace Instruments
 {
     [InstrumentAttribute("KEITHLEY","24")]//24 - BECAUSE 2400,2430 FITS
-    public class Keithley24xx:ISourceMeasurementUnit
+    public class Keithley24xx:AbstractMessageBasedInstrument, ISourceMeasurementUnit
     {
+        public Keithley24xx(string Name,string Alias, string ResourceName):base(Name,Alias,ResourceName)
+        {
+
+        }
+
+        public override bool InitializeDevice()
+        {
+            if(base.InitializeDevice())
+            {
+                SendCommand(":TRIG:COUN 1");
+                SendCommand(":SOUR:CLE:AUTO OFF");
+                SendCommand(":SOUR:FUNC VOLT");
+
+
+            }
+            return false;
+
+        }
         public bool TryReadVoltage(out double Voltage)
         {
             throw new NotImplementedException();
         }
 
         public bool TrySetVoltage(double Voltage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DetectInstrument()
         {
             throw new NotImplementedException();
         }
