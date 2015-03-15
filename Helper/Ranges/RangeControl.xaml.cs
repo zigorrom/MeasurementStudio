f9e7c29d0343648004d3eb85cd8884eb1f4cdd6f
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-03-15 23:37:39 +0100

    Add range control

---
+++ Helper/Ranges/RangeControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Helper.Ranges
+{
+    /// <summary>
+    /// Interaction logic for RangeControl.xaml
+    /// </summary>
+    public partial class RangeControl : UserControl
+    {
+        public RangeControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
