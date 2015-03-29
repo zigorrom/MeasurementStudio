e50ec9205f5833e2eed757bf90c054295c4f05a9
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-03-29 01:27:59 +0100

    123

---
+++ Helper/Ranges/DoubleValueValudationRule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Helper.Ranges
+{
+    public class DoubleValueValudationRule:ValidationRule
+    {
+        public DoubleValueValudationRule():base()
+        {
+
+        }
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            //throw new NotImplementedException();
+            double result = 0;
+            try
+            {
+                string val = value as string;
+                result = double.Parse(val, NumberStyles.Float, new NumberFormatInfo() { NumberDecimalSeparator = ".", NumberGroupSeparator = "" });
+                return new ValidationResult(true, null);
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, e.Message);
+            }
+        }
+    }
+}
