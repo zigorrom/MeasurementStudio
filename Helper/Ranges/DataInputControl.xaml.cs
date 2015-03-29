fac8b9ce6d49667ed1637c91395d1cec10008914
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-03-29 01:07:12 +0100

    123

---
+++ Helper/Ranges/DataInputControl.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Helper
+{
+	/// <summary>
+	/// Interaction logic for DataInputControl.xaml
+	/// </summary>
+	public partial class DataInputControl : UserControl
+	{
+		public DataInputControl()
+		{
+			this.InitializeComponent();
+            
+		}
+        private object m_InputName;
+        public object InputName
+        {
+            get { return m_InputName; }
+            set { m_InputName = value; }
+        }
+
+        private object m_InputValue;
+
+        public object InputValue
+        {
+            get { return m_InputValue; }
+            set { m_InputValue = value; }
+        }
+        
+	}
+}
