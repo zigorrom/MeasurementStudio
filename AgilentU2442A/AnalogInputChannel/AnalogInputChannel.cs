95fe16e355488a9cb8cedcd882b7c7b4c48d5301
zigorrom <zigorrom@gmail.com>
2015-04-23 09:09:37 +0200

    SyncEvents

--- AgilentU2442A/AnalogInputChannel/AnalogInputChannel.cs
+++ AgilentU2442A/AnalogInputChannel/AnalogInputChannel.cs
@@ -20,7 +20,7 @@ namespace AgilentU2442A
             get { return m_PollingParameters; }
         }
 
-        private Queue<double> m_AquiredDataQueue;
+        private Queue<double[]> m_AquiredDataQueue;
         
 
         public AnalogInputChannel(string NativeChannelName, string AliasChannelName, AgilentU2542A ParentDevice):base(NativeChannelName,AliasChannelName,ParentDevice)
---
+++ AgilentU2442A/SyncEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AgilentU2442A
+{
+    public class SyncEvents
+    {
+        private EventWaitHandle m_newItemEvent;
+        private EventWaitHandle m_exitThreadEvent;
+        private WaitHandle[] m_eventArray;
+
+        public SyncEvents()
+        {
+            m_newItemEvent = new AutoResetEvent(false);
+            m_exitThreadEvent = new ManualResetEvent(false);
+            m_eventArray = new WaitHandle[2];
+            m_eventArray[0] = m_newItemEvent;
+            m_eventArray[1] = m_exitThreadEvent;
+        }
+
+        public EventWaitHandle NewItemEvent { get { return m_newItemEvent; } }
+        public EventWaitHandle ExitThreadEvent { get { return m_exitThreadEvent; } }
+        public WaitHandle[] EventArray { get { return m_eventArray; } }
+        
+        
+        
+    }
+}
