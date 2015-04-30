354eea55cca89502988b80917e88fca1185fddef
zigorrom <zigorrom@gmail.com>
2015-04-30 18:19:00 +0200

    TestAgilent

---
+++ AgilentTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using AgilentU2442A;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgilentTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AgilentU2542A ag = new AgilentU2542A("Agilent", "asrdasd","USB0::0x0957::0x1718::TW52524501::0::INSTR");
+        }
+    }
+}
