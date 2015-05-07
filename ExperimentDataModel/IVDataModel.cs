1b11bb1ab2b813f323cf7f3001be24ff6cd8ff61
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-05-08 00:43:46 +0200

    DataVizualization

---
+++ ExperimentDataModel/IVDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentDataModel
+{
+    [Serializable]
+    public class IVDataModel:ISerializable
+    {
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("a", true);
+        }
+    }
+}
--- ExperimentalDataModelTest/Program.cs
+++ ExperimentalDataModelTest/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ExperimentDataModel;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,12 @@ namespace ExperimentalDataModelTest
     {
         static void Main(string[] args)
         {
+            IVDataModel model = new IVDataModel();
+            RawDataFormatter fmt = new RawDataFormatter();
+            using(Stream s = new FileStream(Directory.GetCurrentDirectory()+"\\text.txt",FileMode.Create,FileAccess.Write,FileShare.None))
+            {
+                fmt.Serialize(s, model);
+            }
         }
     }
 }
