4077e8a3e635b42ededeabf39e54710c7f689e00
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-05-08 22:06:41 +0200

    123

--- ExperimentalDataModelTest/Program.cs
+++ ExperimentalDataModelTest/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,22 @@ namespace ExperimentalDataModelTest
     {
         static void Main(string[] args)
         {
-            IVDataModel model = new IVDataModel();
-            RawDataFormatter fmt = new RawDataFormatter();
-            using(Stream s = new FileStream(Directory.GetCurrentDirectory()+"\\text.txt",FileMode.Create,FileAccess.Write,FileShare.None))
-            {
-                fmt.Serialize(s, model);
-            }
+            //IVDataModel model = new IVDataModel();
+            //RawDataFormatter fmt = new RawDataFormatter();
+            //using(Stream s = new FileStream(Directory.GetCurrentDirectory()+"\\text.txt",FileMode.Create,FileAccess.Write,FileShare.None))
+            //{
+            //    fmt.Serialize(s, model);
+            //}
+
+            MailMessage msg = new MailMessage(new MailAddress("i.zadorozhnyi@fz-juelich.de", "a"), new MailAddress("zigorrom@gmail.com", "z"));
+            SmtpClient cli = new SmtpClient();
+            cli.Port = 25;
+            cli.DeliveryMethod = SmtpDeliveryMethod.Network;
+            cli.UseDefaultCredentials = false;
+            cli.Host = "smtp.fz-juelich.de";
+            msg.Subject = "this is a test mail.";
+            msg.Body = "body";
+            cli.Send(msg);
         }
     }
 }
