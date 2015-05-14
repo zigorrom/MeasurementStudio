95df36197c365ef360140598b44e56917519a5a1
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-05-14 14:28:59 +0200

    123

--- Helper/Ranges/DoubleRange/DoubleNumericValue.cs
+++ Helper/Ranges/DoubleRange/DoubleNumericValue.cs
@@ -1,16 +1,32 @@
 ﻿using Helper.Ranges.Units;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Helper.Ranges.DoubleRange
 {
-    public class DoubleNumericValue
+    public class DoubleNumericValue:INotifyPropertyChanged
     {
+        public DoubleNumericValue(IUnits units)
+        {
+            m_NumericValue = 0;
+            m_Magnitude = 0;
+            m_Units = units;
+        }
+
         private double m_NumericValue;
         private double m_Magnitude;
         private IUnits m_Units;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string PropertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(PropertyName));
+        }
     }
 }
