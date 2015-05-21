be190eb4997cc870993a85db2a803220e44fa088
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-05-21 02:24:59 +0200

    123

---
+++ IVCharacterization/DataModel/DataPropertyAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IVCharacterization.DataModel
+{
+    public class DataPropertyAttribute:Attribute
+    {
+        public DataPropertyAttribute(bool isIndependent, bool isValid, int lastCalculatedRow, string propertyName, string propertyUnits, string propertyComments)
+        {
+            IsIndependent = isIndependent;
+            IsValid = isValid;
+            LastCalculatedRow = lastCalculatedRow;
+            PropertyName = propertyName;
+            PropertyUnits = propertyUnits;
+            PropertyComments = propertyComments;
+        }
+        public bool IsIndependent { get; private set; }
+        public bool IsValid { get; private set; }
+        public int LastCalculatedRow { get;private set; }
+        public string PropertyName { get; private set; }
+        public string PropertyUnits { get; private set; }
+        public string PropertyComments { get; private set; }
+    }
+}
---
+++ IVCharacterization/DataModel/DrainSourceDataRow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IVCharacterization.DataModel
+{
+    internal struct DrainSourceDataRow
+    {
+        [DataPropertyAttribute(true, true, -1, "V\\_DS", "DrainSourceVoltage", "V")]
+        public double DrainSourceVoltage;
+        [DataPropertyAttribute(true, true, -1, "I\\_D", "DrainCurrent", "A")]
+        public double DrainCurrent;
+        [DataPropertyAttribute(true, true, -1, "I\\_G", "GateCurrent", "A")]
+        public double GateCurrent;
+
+        public DrainSourceDataRow(double drainSourceVoltage,double drainCurrent, double gateCurrent)
+        {
+            DrainSourceVoltage = drainSourceVoltage;
+            DrainCurrent = drainCurrent;
+            GateCurrent = gateCurrent;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}\t{1}\t{2}");
+        }
+    }
+}
