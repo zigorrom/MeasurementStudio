0f6b5c26cf52e787575c5672830905b144b5828c
zigorrom <zigorrom@gmail.com>
2015-06-18 19:50:37 +0200

    123

--- FFT/Complex.cpp
+++
@@ -1,5 +0,0 @@
-#include "Complex.h"
-
-inline void ShortComplex::operator=(const Complex &y) { re = (double)y.re; im = (double)y.im; }
-inline void Complex::operator=(const Complex &y) { re = y.re; im = y.im; }
-inline void Complex::operator=(const ShortComplex &y) { re = y.re; im = y.im; }
