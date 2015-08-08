5cd207215e8838d8656664c3e42c30214c2cd9fb
zigorrom <zigorrom@gmail.com>
2015-08-08 19:24:40 +0200

    123

--- OxyDataVisualization/OxyMainView.cs
+++ OxyDataVisualization/OxyMainView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OxyPlot;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,23 @@ namespace OxyDataVisualization
         public string Title { get; set; }
         public string Subtitle { get; set; }
         public string ExpressionToDisplay { get; set; }
+        public List<List<DataPoint>> source { get; set; }
 
+        public OxyMainView()
+        {
+            Title = "title";
+            Subtitle = "subtitle";
+            source = new List<List<DataPoint>>();
+            for (int i = 0; i < 3; i++)
+            {
+                var a = new List<DataPoint>();
+                
+                for (int j = 0; j < 5; j++)
+                {
+                    a.Add(new DataPoint(i, i * j));
+                }
+                source.Add(a);
+            }
+        }
     }
 }
