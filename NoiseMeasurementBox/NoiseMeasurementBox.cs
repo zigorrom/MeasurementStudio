4c9ff564c8bcbae6a56688ded9579e0df08b5a9e
zigorrom <zigorrom@gmail.com>
2015-08-08 16:14:50 +0200

    123

--- NoiseMeasurementBox/NoiseMeasurementBox.cs
+++ NoiseMeasurementBox/NoiseMeasurementBox.cs
@@ -6,11 +6,48 @@ using System.Threading.Tasks;
 
 namespace NoiseMeasurementBox
 {
-    public 
+    using Agilent.AgilentU254x.Interop;
 
-    public class NoiseMeasurementBox
+    public class NoiseMeasurementBox:IDisposable
     {
-        public 
+        private AgilentU254xClass _agilent;
+        private DataRouter _router;
+
+        
+        public bool Initialized {
+            get
+            {
+                return _agilent.Initialized;
+            }
+        }
+
+
+        public NoiseMeasurementBox()
+        {
+            _agilent = new AgilentU254xClass();
+        }
+
+        public void Initialize(string ResourceName, bool IdQuery, bool Reset  )
+        {
+            _agilent.Initialize(ResourceName, IdQuery, Reset);
+        }
+
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetDrainSourceVoltage(double Voltage)
+        {
+            return false;
+        }
+
+        public bool SetGateVoltage(double Voltage)
+        {
+            return false;
+        }
+
 
     }
 }
