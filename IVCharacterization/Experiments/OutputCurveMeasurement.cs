e28fdbd64d735a987164946c8ba92048fd735d49
zigorrom <zigorrom@gmail.com>
2015-08-14 18:14:09 +0200

    123

---
+++ IVCharacterization/Experiments/OutputCurveMeasurement.cs
@@ -0,0 +1,66 @@
+﻿using ExperimentAbstraction;
+using IVCharacterization.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IVCharacterization.Experiments
+{
+    class OutputCurveMeasurement : AbstractExperiment<DrainSourceMeasurmentInfoRow, DrainSourceDataRow>
+    {
+
+        
+
+        public OutputCurveMeasurement():base("Output curve measurement")
+        {
+
+        }
+
+        public override void InitializeExperiment()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InitializeInstruments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ReleaseInstruments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int ReportProgress()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Abort()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OwnInstruments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object ViewModel
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override System.Windows.Controls.UserControl Control
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
