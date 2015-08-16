d788df16c94c50ad554ed80e6080f08987a77390
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-08-16 23:14:05 +0200

    123

---
+++ ExperimentDataModel/Exporter/AbstractMeasurementDataExporter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentDataModel
+{
+    public abstract class AbstractMeasurementDataExporter<InfoT,DataT>:IMeasurementDataExporter<InfoT,DataT>
+        where InfoT:struct
+        where DataT:struct
+    {
+        public AbstractMeasurementDataExporter()
+        {
+
+        }
+
+
+        private string _workingDirectory;
+        public string WorkingDirectory
+        {
+            get { return _workingDirectory; }
+        }
+
+        public void NewExperiment(string WorkingFolder)
+        {
+            _workingDirectory = WorkingFolder;
+        }
+
+        public abstract void Write(MeasurementData<InfoT, DataT> measurement);
+
+
+        public abstract void WriteDelayed(MeasurementData<InfoT, DataT> measurement);
+        
+    }
+}
---
+++ ExperimentDataModel/Exporter/IMeasurementDataExporter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExperimentDataModel
+{
+    public interface IMeasurementDataExporter<InfoT,DataT>
+        where InfoT:struct
+        where DataT:struct
+    {
+        string WorkingDirectory { get;  }
+        void NewExperiment(string WorkingFolder);
+
+        void Write(MeasurementData<InfoT, DataT> measurement);
+        void WriteDelayed(MeasurementData<InfoT, DataT> measurement);
+    }
+}
