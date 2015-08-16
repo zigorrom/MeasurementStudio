2aabedfe0fb60a5f215376982bdfaec40702fb29
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-08-16 22:57:47 +0200

    123

--- ExperimentDataModel/DatabaseExporter/DataBaseMeasurementDataExporter.cs
+++ ExperimentDataModel/DatabaseExporter/DataBaseMeasurementDataExporter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ExperimentDataModel
 {
-    public abstract class MeasurementDataExporter
+    public class DataBaseMeasurementDataExporter
     {
 
     }
--- ExperimentDataModel/StreamExporter/StreamMeasurementDataExporter.cs
+++ ExperimentDataModel/StreamExporter/StreamMeasurementDataExporter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ExperimentDataModel
 {
-    public interface IMeasurementDataExporter
+    public class StreamMeasurementDataExporter
     {
 
     }
