ec839851975a3ae78f70655230fb19dfee06bf06
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-08-22 13:35:50 +0200

    123

---
+++ DataVisualization/D3DataVisualization/GraphTypeToAxisProviderConverter.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Research.DynamicDataDisplay.Charts;
+using Microsoft.Research.DynamicDataDisplay.Charts.Axes.Numeric;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DataVisualization.D3DataVisualization
+{
+    public enum AxisType
+    {
+        Horizontal,
+        Vertical
+    }
+
+    [ValueConversion(typeof(GraphScaleType), typeof(NumericAxis))]
+    public class GraphTypeToAxisConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+     
+            return false;//((Visibility)value) == Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return true;///((bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
