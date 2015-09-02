ea121daaf339ea6d16df41edb7eb3e4ec1d9ff4d
zigorrom <zigorrom@gmail.com>
2015-09-02 12:14:09 +0200

    123

--- IVCharacterization/ViewModels/IVexpSettingsViewModel.cs
+++ IVCharacterization/ViewModels/IVexpSettingsViewModel.cs
@@ -1,14 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IVCharacterization.ViewModels
 {
-    public class IVexpSettingsViewModel
+    public class IVexpSettingsViewModel:INotifyPropertyChanged
     {
-        public bool UseSampleSelector { get; set; }
+        #region PropertyEvents
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected bool SetField<ST>(ref ST field, ST value, string propertyName)
+        {
+            if (EqualityComparer<ST>.Default.Equals(field, value))
+                return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        private void OnPropertyChanged(string PropertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        #endregion
+        private bool _useSampleSelector;
+        public bool UseSampleSelector
+        {
+            get { return _useSampleSelector; }
+            set
+            {
+                SetField(ref _useSampleSelector, value, "UseSampleSelector");
+            }
+
+        }
 
     }
 }
