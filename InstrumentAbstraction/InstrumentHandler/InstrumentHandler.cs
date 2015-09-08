cffe8be8b48edf0dfc13a193fac62f7c764081c4
zigorrom <zigorrom@gmail.com>
2015-09-08 18:01:34 +0200

    123

--- InstrumentAbstraction/InstrumentHandler/InstrumentHandler.cs
+++ InstrumentAbstraction/InstrumentHandler/InstrumentHandler.cs
@@ -24,39 +24,11 @@ namespace InstrumentHandlerNamespace
     public sealed class InstrumentHandler//:NotifyPropertyChanged
     {
         private static volatile InstrumentHandler _handler;
+
         private static object syncRoot = new object();
-        private const string SerializationFileName = "Devices.ser";
-        private const string ResourceFilter = "(GPIB)|(USB)|(COM)?*INSTR";
-
-        //private Dictionary<string,IInstrument> m_InstrumentList;
-        //private List<IInstrument> m_Instruments;
-        //private ObservableCollection<IInstrument> _instruments;
-        //public ObservableCollection<IInstrument> Instruments
-        //{
-        //    get { return _instruments; }
-        //}
-        private void InitializeHandler()
-        {
-
-            //if (_instruments == null)
-            //    _instruments = new ObservableCollection<IInstrument>();
-
-            DiscoverInstruments();
-            CheckInstrumentsConnectivity();
-
-            //InitializeViewModel();
-        }
-
-
-        private void CheckInstrumentsConnectivity()
-        {
-            //foreach (var instr in _instruments)
-            //{
-            //    //if(instr.Is)
-            //}
-
-        }
-
+        
+        private const string SerializationFileName = "Devices.seri";
+        private const string ResourceFilter = "(GPIB)|(USB)|(COM)?*INSTR";//"?*INSTR";
 
         // For serialization default constructor necessary
         private InstrumentHandler()
@@ -68,13 +40,13 @@ namespace InstrumentHandlerNamespace
         ~InstrumentHandler()
         {
             //if(NeedSerialization)
-            var dir = Directory.GetCurrentDirectory();
-            var binFormatter = new BinaryFormatter();
-            using (Stream stream = new FileStream(String.Format("{0}\\{1}", dir, SerializationFileName), FileMode.Create, FileAccess.Write, FileShare.None))
-            {
-                binFormatter.Serialize(stream, this);
-                stream.Close();
-            }
+            //var dir = Directory.GetCurrentDirectory();
+            //var binFormatter = new BinaryFormatter();
+            //using (Stream stream = new FileStream(String.Format("{0}\\{1}", dir, SerializationFileName), FileMode.Create, FileAccess.Write, FileShare.None))
+            //{
+            //    binFormatter.Serialize(stream, this);
+            //    stream.Close();
+            //}
 
         }
 
@@ -90,78 +62,73 @@ namespace InstrumentHandlerNamespace
                 return _handler;
             }
         }
+        
+        private void InitializeHandler()
+        {
+            
+
+            InitializeProperties();
+
+            InitPlugins();
+            
+            DiscoverInstruments();
+            
+            CheckInstrumentsConnectivity();
+
+            
+        }
+
+        private void InitializeProperties()
+        {
+            Instruments = new List<IInstrument>();
+        }
 
         private static InstrumentHandler DeserializeOrNew()
         {
-            InstrumentHandler handler;
-            try
-            {
-                var dir = Directory.GetCurrentDirectory();
-                var binFormatter = new BinaryFormatter();
-                using (Stream stream = new FileStream(String.Format("{0}\\{1}", dir, SerializationFileName), FileMode.Open, FileAccess.Read, FileShare.None))
-                {
-                    handler = (InstrumentHandler)binFormatter.Deserialize(stream);
-                    stream.Close();
-                }
-            }
-            catch (Exception ex)
-            {
-                handler = new InstrumentHandler();
-            }
-            if (null == handler)
-                handler = new InstrumentHandler();
+            
+            //InstrumentHandler handler;
+            //try
+            //{
+            //    var dir = Directory.GetCurrentDirectory();
+            //    var binFormatter = new BinaryFormatter();
+            //    using (Stream stream = new FileStream(String.Format("{0}\\{1}", dir, SerializationFileName), FileMode.Open, FileAccess.Read, FileShare.None))
+            //    {
+            //        handler = (InstrumentHandler)binFormatter.Deserialize(stream);
+            //        stream.Close();
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    handler = new InstrumentHandler();
+            //}
+            //if (null == handler)
+            //    handler = new InstrumentHandler();
 
-            handler.InitializeHandler();
-            return handler;
+            //handler.InitializeHandler();
+            //return handler;
+
+            var ihandler = new InstrumentHandler();
+
+            ihandler.InitializeHandler();
+            return _handler;
         }
 
-        
-
-        private void DiscoverInstruments()
+        private void InitPlugins()
         {
-            ///
-            /// CREATE ATTRIBUTES THAT ALLOWS TO DETERMINE WHICH CLASS CAN BE SUITABLE TO USE INSTRUMENT
-            ///
-
-                //var LocalResourceManager = ResourceManager.GetLocalManager();
-                //var resources = LocalResourceManager.FindResources(ResourceFilter);
-                //if (resources.Length == 0)
-                //{
-                //    throw new Exception("No instruments found");
-                //}
-                //foreach (var resource in resources)
-                //{
-                //    var s = (MessageBasedSession)LocalResourceManager.Open(resource);
-                //    s.Write("*IDN?");
-                //    var idn = s.ReadString();
-                //    s.Dispose();
-                //    foreach (var item in types)
-                //    {
-                //        if (!item.Key.FitsToIDN(idn))
-                //            continue;
-                //        var instr = (IInstrument)Activator.CreateInstance(item.Value, String.Format("Manufacturer:{0},Model:{1}", item.Key.Manufacturer, item.Key.Model), "", resource);
-                //        m_Instruments.Add(instr);
-                //    }
-                //}
-        
-
             try
             {
                 var aggregateCatalog = new AggregateCatalog();
                 var directory = string.Concat(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
                                 .Split('\\').Reverse().Skip(3).Reverse().Aggregate((a, b) => a + "\\" + b)
                                 , "\\", "Instrument\\Components");
-                
+
                 ///
                 ///ADD HERE SECURITY KEY CHECK
                 /// 
                 ///
 
-
-
-
                 var directoryCatalog = new DirectoryCatalog(directory, "*.dll");
-                
+
                 var asmCatalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
                 aggregateCatalog.Catalogs.Add(directoryCatalog);
                 aggregateCatalog.Catalogs.Add(asmCatalog);
@@ -169,20 +136,80 @@ namespace InstrumentHandlerNamespace
                 var container = new CompositionContainer(aggregateCatalog);
                 container.ComposeParts(this);
 
-                foreach (var i in Instruments)
+                foreach (var i in InstrumentFactoriesPlugins)
                 {
                     var a = i.Value;
-                   var instr =  a.CreateInstrument("", "", "");
+                    var instr = a.CreateInstrument("", "", "");
                 }
-               
+
             }
             catch (Exception ex)
             {
-
                 throw ex;
             }
+        }
+
+        public List<IInstrument> Instruments { get; private set; }
+
+        private void DiscoverInstruments()
+        {
+            ///
+            /// CREATE ATTRIBUTES THAT ALLOWS TO DETERMINE WHICH CLASS CAN BE SUITABLE TO USE INSTRUMENT
+            ///
+            try
+            {
+                var LocalResourceManager = ResourceManager.GetLocalManager();
+                var resources = LocalResourceManager.FindResources(ResourceFilter);
+                if (resources.Length == 0)
+                {
+                    throw new Exception("No instruments found");
+                }
+
+
+                foreach (var resource in resources)
+                {
+                    var s = (MessageBasedSession)LocalResourceManager.Open(resource);
+                    var idn = s.Query("*IDN?");
+
+                    var factory = InstrumentFactoriesPlugins.Where(x => x.Value.FitsIDN(idn)).Select(x=>x.Value).FirstOrDefault();
+                    if (factory == default(IInstrumentFactory))
+                        continue;
+                    
+                    //Instruments.Add(factory.CreateInstrument()
+                    //s.Write("*IDN?");
+                    //var idn = s.ReadString();
+                    //s.Dispose();
+                    //foreach (var item in types)
+                    //{
+                    //    if (!item.Key.FitsToIDN(idn))
+                    //        continue;
+                    //    var instr = (IInstrument)Activator.CreateInstance(item.Value, String.Format("Manufacturer:{0},Model:{1}", item.Key.Manufacturer, item.Key.Model), "", resource);
+                    //    m_Instruments.Add(instr);
+                    //}
+                }
+            }catch(Exception ex)
+            {
+
+            }
+
+
 
         }
+
+        private void CheckInstrumentsConnectivity()
+        {
+           
+
+        }
+
+
+       
+
+      
+
+        
+
+        
 
         public bool TryGetDevice(string InstrumentName, out IInstrument Instrument, IInstrumentOwner Owner)
         {
@@ -201,8 +228,6 @@ namespace InstrumentHandlerNamespace
 
 
         [ImportMany]
-        //public Lazy<IInstrumentFactory>[] factories { get; set; }
-        //public IEnumerable<IInstrument> Instruments { get; set; }
-        private Lazy<IInstrumentFactory, IDictionary<string, object>>[] Instruments { get; set; }
+        private Lazy<IInstrumentFactory, IDictionary<string, object>>[] InstrumentFactoriesPlugins { get; set; }
     }
 }
