289023071d04c4258689cef845066a8a9a7eb493
zigorrom <zigorrom@gmail.com>
2015-09-11 15:19:50 +0200

    TestTemplate

---
+++ ExperimentProjectTemplate/NewExperiment/Experiments/Experiment.cs
@@ -0,0 +1,28 @@
+﻿using NewExperiment.DataModel;
+using NewExperiment.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewExperiment.Experiments
+{
+    public sealed class Experiment:MeasurementBase<MeasurementInfoRow,MeasurementDataRow>
+    {
+        public Experiment(ExperimentMainViewModel viewModel):base(viewModel, "Measurement name")
+        {
+
+        }
+
+        protected override void DoMeasurement(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SimulateMeasurement(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
