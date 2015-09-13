b0fa88b19ebff6240ba876240ed111a7c346e09a
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2015-09-13 21:31:56 +0200

    123

--- ExperimentDataModel/Exporter/OriginExporter/OriginMeasurementDataExporter.cs
+++ ExperimentDataModel/Exporter/OriginExporter/OriginMeasurementDataExporter.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace ExperimentDataModel
 {
-    public class OriginMeasurementDataExporter
+    public class OriginMeasurementDataExporter<InfoT, DataT>:IMeasurementDataExporter<InfoT,DataT>
+        where InfoT : struct, IMeasurementInfo
+        where DataT : struct
     {
+        public string WorkingDirectory
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public void NewExperiment(string WorkingFolder)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(MeasurementData<InfoT, DataT> measurement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
