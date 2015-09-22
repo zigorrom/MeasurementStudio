43fc25ee0f5c339a67865375af259dfb360796db
zigorrom <zigorrom@gmail.com>
2015-09-22 17:26:38 +0200

    ExpandableTabControl+NoiseMeasurementExperiment

---
+++ NoiseMeasurementLegacy/ViewModels/ExperimentMainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ExperimentAbstraction;
+using Helper.NewExperimentWindow;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoiseMeasurementLegacy.ViewModels
+{
+    public abstract class ExperimentMainViewModel : AbstractExperimentViewModel
+    {
+        public ExperimentMainViewModel()
+        {
+            SettingsViewModel = new ExperimentSettingsViewModel();
+        }
+
+        public ExperimentSettingsViewModel SettingsViewModel { get; set; }
+
+        
+    }
+}
