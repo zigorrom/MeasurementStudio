4f0549e8ff37327af177dcbfb5159d2497c856f1
zigorrom <zigorrom@gmail.com>
2015-12-04 09:16:43 +0100

    123

--- ExperimentDataModel/ObservableMeasurementData.cs
+++ ExperimentDataModel/ObservableMeasurementData.cs
@@ -17,7 +17,6 @@ namespace ExperimentDataModel
         public ObservableMeasurementData(InfoT info)
         {
             Info = info;
-            
         }
 
         private InfoT _info;
@@ -29,9 +28,25 @@ namespace ExperimentDataModel
         }
 
 
+        private bool _collectionChanged = false;
+        private bool _updatesEnabled = true;
         public event EventHandler DataChanged;
 
+        private Func<DataT, Point> xyMapping;
+        private Func<DataT, double> xMapping;
+        private Func<DataT, double> yMapping;
+        private object SyncRoot = new object();
 
+
+
+
+
+
+        protected override void OnCollectionChanged(System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        {
+            base.OnCollectionChanged(e);
+            
+        }
 
         public IDisposable Subscribe(IObserver<DataT[]> observer)
         {
