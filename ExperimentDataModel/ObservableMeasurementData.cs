9530d2dff94c075938178c0c542ac7ef720ff5a0
zigorrom <zigorrom@gmail.com>
2015-12-04 14:17:22 +0100

    123

--- ExperimentDataModel/ObservableMeasurementData.cs
+++ ExperimentDataModel/ObservableMeasurementData.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,29 +28,97 @@ namespace ExperimentDataModel
             private set { _info = value; }
         }
 
-
-        private bool _collectionChanged = false;
-        private bool _updatesEnabled = true;
         public event EventHandler DataChanged;
 
         private Func<DataT, Point> xyMapping;
         private Func<DataT, double> xMapping;
         private Func<DataT, double> yMapping;
+
+        private readonly ObservableCollection<DataT> _measurementCollection = new ObservableCollection<DataT>();
         private object SyncRoot = new object();
 
+        private int _refreshEveryNpoints;
 
-
-
-
-
-        protected override void OnCollectionChanged(System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+        public int RefreshEveryNpoint
         {
-            base.OnCollectionChanged(e);
+            get { return _refreshEveryNpoints; }
+            set { _refreshEveryNpoints = value; }
+        }
+
+        private int _saveRequestEveryNpoints;
+
+        public int SaveRequestEveryNpoints
+        {
+            get { return _saveRequestEveryNpoints; }
+            set { _saveRequestEveryNpoints = value; }
+        }
+
+        private int _displayPointsWindow;
+
+        public int DisplayPointsWindow
+        {
+            get { return _displayPointsWindow; }
+            set { _displayPointsWindow = value; }
+        }
+        
+
+        private bool _collectionChanged = false;
+
+        private bool _updatesEnabled = true;
+
+        public bool UpdatesEnabled
+        {
+            get { return _updatesEnabled; }
+            private set { _updatesEnabled = value; }
+        }
+
+        public void SuspendUpdate()
+        {
+            UpdatesEnabled = false;
+        }
+
+        public void ResumeUpdate()
+        {
+            UpdatesEnabled = true;
+            if (_collectionChanged)
+            {
+                _collectionChanged = false;
+                RaiseDataChanged();
+            }
+        }
+
+
+        private int _updatePointsCounter = 0;
+
+
+        private void RaiseDataChanged()
+        {
+            if (_updatePointsCounter < RefreshEveryNpoint)
+                return;
             
+            _updatePointsCounter = 0;
+            var handler = DataChanged;
+            if (handler != null)
+            {
+                handler(this, EventArgs.Empty);
+            }
+
+        }
+
+        protected override void OnCollectionChanged(NotifyCollectionChangedEventArgs e)
+        {
+            if (UpdatesEnabled)
+            {
+                base.OnCollectionChanged(e);
+                RaiseDataChanged();
+            }
+            else
+                _collectionChanged = true;
         }
 
         public IDisposable Subscribe(IObserver<DataT[]> observer)
         {
+            
             throw new NotImplementedException();
         }
 
@@ -57,6 +126,7 @@ namespace ExperimentDataModel
 
         public IPointEnumerator GetEnumerator(DependencyObject context)
         {
+            
             throw new NotImplementedException();
         }
 
