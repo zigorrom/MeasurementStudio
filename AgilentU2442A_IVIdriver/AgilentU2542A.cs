c999351abf4f92c00c8d6d0b16b9882730554764
zigorrom <zigorrom@gmail.com>
2016-09-06 23:52:01 +0200

    Threading for aquisition

--- AgilentU2442A_IVIdriver/AgilentU2542A.cs
+++ AgilentU2442A_IVIdriver/AgilentU2542A.cs
@@ -156,9 +156,60 @@ namespace AgilentU2442A_IVIdriver
             throw new NotImplementedException();
         }
 
+
+        #region IDisposable Members
+
+        /// <summary>
+        /// Internal variable which checks if Dispose has already been called
+        /// </summary>
+        private Boolean disposed;
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        protected virtual void Dispose(Boolean disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                _router.Dispose();
+                Driver.Close();
+                //TODO: Managed cleanup code here, while managed refs still valid
+            }
+            //TODO: Unmanaged cleanup code here
+
+            disposed = true;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
         public void Dispose()
         {
-            throw new NotImplementedException();
+            // Call the private Dispose(bool) helper and indicate 
+            // that we are explicitly disposing
+            this.Dispose(true);
+
+            // Tell the garbage collector that the object doesn't require any
+            // cleanup when collected since Dispose was called explicitly.
+            GC.SuppressFinalize(this);
         }
+
+        /// <summary>
+        /// The destructor for the class.
+        /// </summary>
+        ~AgilentU2542A()
+        {
+            this.Dispose(false);
+        }
+
+
+        #endregion
+        
     }
 }
