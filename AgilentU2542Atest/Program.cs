bc67c576d73228004bbaf074ccca310978b80e55
zigorrom <zigorrom@gmail.com>
2017-01-31 09:27:17 +0100

    123

--- AgilentU2542Atest/Program.cs
+++ AgilentU2542Atest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AgilentU2442A;
+using NationalInstruments.VisaNS;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,18 +12,48 @@ namespace AgilentU2542Atest
     {
         static void Main(string[] args)
         {
+            MessageBasedSession session = new MessageBasedSession("ADC");
+            session.TerminationCharacterEnabled = true;
+            session.TerminationCharacter = 0xA;
+            session.DefaultBufferSize = 400100;
+            session.Write("*RST");
+            session.Write("ROUT:ENAB ON,(@101:104)");
+            session.Write("ACQ:SRAT 500000");
+            session.Write("ACQ:POIN 50000");
+            session.Write("WAV:POIN 50000");
 
-            var agilent = new AgilentU2542A("asdasd", "", "ADC");
-            var ch1 = agilent.GetAnalogInputChannel(ChannelEnum.AI_CH101);
-            ch1.ChannelEnable = ChannelEnableEnum.Enabled;
+            session.Write("RUN");
+            int counter = 0;
+            int cycles = 10000;
+            string status = string.Empty;
+            string data = string.Empty;
+            while(counter<cycles)
+            {
+                session.Write("WAV:STAT?");
+                status = session.ReadString();
+                if (status == "DATA\n")
+                {
+                    data = session.Query("WAV:DATA?");
+                    Console.WriteLine("data length {0}", data.Length);
+                }
+                Console.WriteLine(status);
+                
 
-            ch1.SampleRate = 500000;
-            ch1.PointsPerShot = 50000;
-            ch1.DataSetReady += ch1_DataSetReady;
-            Console.WriteLine(agilent.Query(agilent.CommandSet.IDNQuery()));
-            ch1.StartAcquisition();
-            System.Threading.Thread.Sleep(100000);
-            ch1.StopAcquisition();
+                counter++;
+            }
+            session.Write("STOP");
+            Console.ReadKey();
+            //var agilent = new AgilentU2542A("asdasd", "", "ADC");
+            //var ch1 = agilent.GetAnalogInputChannel(ChannelEnum.AI_CH101);
+            //ch1.ChannelEnable = ChannelEnableEnum.Enabled;
+
+            //ch1.SampleRate = 500000;
+            //ch1.PointsPerShot = 50000;
+            //ch1.DataSetReady += ch1_DataSetReady;
+            //Console.WriteLine(agilent.Query(agilent.CommandSet.IDNQuery()));
+            //ch1.StartAcquisition();
+            //System.Threading.Thread.Sleep(100000);
+            //ch1.StopAcquisition();
             //for (int i = 0; i < 0xFFFFFFFE; i++)
             //{
             //ss
