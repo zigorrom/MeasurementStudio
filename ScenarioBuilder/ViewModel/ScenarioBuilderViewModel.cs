0deab3daacf321db711bed7359af9ce74c4aefca
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2017-04-19 01:59:08 +0200

    123

--- ScenarioBuilder/ViewModel/ScenarioBuilderViewModel.cs
+++ ScenarioBuilder/ViewModel/ScenarioBuilderViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IVexperiment.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,13 +7,80 @@ using System.Threading.Tasks;
 
 namespace ScenarioBuilder.ViewModel
 {
+    public interface IExperimentItem
+    {
+        string Name { get; }
+        object GenerateViewModel();
+    }
+
+    public class ExperimentItem:IExperimentItem
+    {
+        public ExperimentItem()
+        {
+            expType = null;
+        }
+        Type expType;
+
+        public void SetExperiment<T>()
+        {
+            expType = typeof(T);
+            Name = expType.Name;
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+        public object GenerateViewModel()
+        {
+            return Activator.CreateInstance(expType);
+        }
+    }
+    public class ExperimentItem<ExperimentType>:IExperimentItem
+        where ExperimentType:class, new()
+    {
+        public ExperimentItem()
+        {
+            var t = typeof(ExperimentType);
+            Name = "afasgfashf"+t.Name;
+
+        }
+
+        public string Name
+        {
+            get;
+            private set;
+        }
+        public object GenerateViewModel()
+        {
+            return new ExperimentType();
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
     public class ScenarioBuilderViewModel
     {
         public ScenarioBuilderViewModel()
         {
-
+            AvailableExperiments = new List<IExperimentItem>();
+            InitializeAvailableExperiments();    
         }
 
+        private void InitializeAvailableExperiments()
+        {
+            var item = new ExperimentItem();
+            item.SetExperiment<OutputIVViewModel>();
+            AvailableExperiments.Add(item);
+            //AvailableExperiments.Add(new ExperimentItem<OutputIVViewModel>());
+            //AvailableExperiments.Add(new ExperimentItem<TransfrerIVViewModel>());
+        }
+
+        public List<IExperimentItem> AvailableExperiments { get; private set; }
         
 
     }
--- ScenarioBuilder/Views/MainWindow.xaml.cs
+++ ScenarioBuilder/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ScenarioBuilder.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF.JoshSmith.ServiceProviders.UI;
 
 namespace ScenarioBuilder
 {
@@ -20,10 +22,90 @@ namespace ScenarioBuilder
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        ListViewDragDropManager<IExperimentItem> dragMgr;
+        ListViewDragDropManager<IExperimentItem> dragMgr2;
         public MainWindow()
         {
             InitializeComponent();
+
+            this.dragMgr = new ListViewDragDropManager<IExperimentItem>(this.AvailableExperimentList);
+            this.dragMgr2 = new ListViewDragDropManager<IExperimentItem>(this.ScenarioList);
+            this.AvailableExperimentList.Drop += OnListDrop;
+            this.AvailableExperimentList.DragEnter += OnListDragEnter;
+            this.ScenarioList.Drop += OnListDrop;
+            this.ScenarioList.DragEnter += OnListDragEnter;
+            this.dragMgr.ProcessDrop += dragMgr_ProcessDrop;
+            this.dragMgr2.ProcessDrop += dragMgr_ProcessDrop;
         }
+
+        private void OnListDragEnter(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.Move;
+        }
+
+        void dragMgr_ProcessDrop(object sender, ProcessDropEventArgs<IExperimentItem> e)
+        {
+            // This shows how to customize the behavior of a drop.
+            // Here we perform a swap, instead of just moving the dropped item.
+
+            int higherIdx = Math.Max(e.OldIndex, e.NewIndex);
+            int lowerIdx = Math.Min(e.OldIndex, e.NewIndex);
+
+            if (lowerIdx < 0)
+            {
+                // The item came from the lower ListView
+                // so just insert it.
+                e.ItemsSource.Insert(higherIdx, e.DataItem);
+            }
+            else
+            {
+                // null values will cause an error when calling Move.
+                // It looks like a bug in ObservableCollection to me.
+                if (e.ItemsSource[lowerIdx] == null ||
+                    e.ItemsSource[higherIdx] == null)
+                    return;
+
+                // The item came from the ListView into which
+                // it was dropped, so swap it with the item
+                // at the target index.
+                e.ItemsSource.Move(lowerIdx, higherIdx);
+                e.ItemsSource.Move(higherIdx - 1, lowerIdx);
+            }
+
+            // Set this to 'Move' so that the OnListViewDrop knows to 
+            // remove the item from the other ListView.
+            e.Effects = DragDropEffects.Move;
+        }
+
+        private void OnListDrop(object sender, DragEventArgs e)
+        {
+            if (e.Effects == DragDropEffects.None)
+                return;
+
+            var experimentItem = e.Data.GetData(typeof(IExperimentItem)) as IExperimentItem;
+            //if (sender == this.AvailableExperimentList)
+            //{
+            //    if (this.dragMgr.IsDragInProgress)
+            //        return;
+
+            //    // An item was dragged from the bottom ListView into the top ListView
+            //    // so remove that item from the bottom ListView.
+            //    //(this.listView2.ItemsSource as ObservableCollection<Task>).Remove(task);
+            //}
+            //else
+            //{
+            //    if (this.dragMgr2.IsDragInProgress)
+            //        return;
+
+            //    // An item was dragged from the top ListView into the bottom ListView
+            //    // so remove that item from the top ListView.
+            //    (this.listView.ItemsSource as ObservableCollection<Task>).Remove(task);
+            //}
+        }
+
+        
+        
 
 
     }
