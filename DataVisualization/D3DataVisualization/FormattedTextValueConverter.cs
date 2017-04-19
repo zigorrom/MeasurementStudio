67f09da8510c3a15f92a9ef393326d543b6658f0
zigorrom <https://github.com/zigorrom/DigitalAnalyzer.git>
2017-04-20 00:22:57 +0200

    123

--- DataVisualization/D3DataVisualization/FormattedTextValueConverter.cs
+++ DataVisualization/D3DataVisualization/FormattedTextValueConverter.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
+using System.Windows.Documents;
 
 namespace DataVisualization.D3DataVisualization
 {
@@ -11,16 +12,27 @@ namespace DataVisualization.D3DataVisualization
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return value;
+            //throw new NotImplementedException();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
+            var a = (string)value;
+            var par = new Paragraph();
+            par.Inlines.Add("dfasfasf");
+            par.Inlines.Add(a);
+            return par;
+//            <Paragraph FontFamily="Palatino Linotype">
+//            2<Run Typography.Variants="Superscript">3</Run>
+//            14<Run Typography.Variants="Superscript">th</Run>
+//            </Paragraph>
+
             //from string convertion
             //TextRange selection = new TextRange(document.ContentStart, document.ContentEnd);
             //selection.ApplyPropertyValue(Inline.BaselineAlignmentProperty, BaselineAlignment.Superscript);
             //throw new NotImplementedException();
-            return value;
+            //return value;
         }
     }
 }
