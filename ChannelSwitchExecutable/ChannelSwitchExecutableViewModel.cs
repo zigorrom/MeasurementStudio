4de5bae65c0b1afc1956f422b6fe38138d67dd41
zigorrom <zigorrom@gmail.com>
2017-04-20 19:01:49 +0200

    123

--- ChannelSwitchExecutable/ChannelSwitchExecutableViewModel.cs
+++ ChannelSwitchExecutable/ChannelSwitchExecutableViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using ExperimentAbstraction;
+using Microsoft.TeamFoundation.MVVM;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace ChannelSwitchExecutable
 {
@@ -34,7 +37,22 @@ namespace ChannelSwitchExecutable
             get { throw new NotImplementedException(); }
         }
 
+        private ICommand _buttonPressed;
+        public ICommand ButtonPressed
+        {
+            get
+            {
+                return _buttonPressed ?? (_buttonPressed = new RelayCommand((b) =>
+                {
+                    var button = (Button)b;
+                    var channelNumber = int.Parse(button.Content.ToString());
+                    //var task = new MeasurementChannelController(channelNumber);
+                    //this.SelectedMeasurementChannels.Add(task);
 
+                    //System.Windows.MessageBox.Show(String.Format("Button pressed {0}",button.Content));
+                }));//new RoutedUICommand("keyInput", "keyPressed", typeof(IMainViewModel)));
+            }
+        } 
 
     }
 }
