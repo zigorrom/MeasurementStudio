3304a73136d5c8fb49f4457455b8beca5c07315c
zigorrom <zigorrom@gmail.com>
2017-04-28 20:47:41 +0200

    123

--- MeasurementStudio/App.xaml.cs
+++ MeasurementStudio/App.xaml.cs
@@ -14,12 +14,12 @@ namespace MeasurementStudio
     public partial class App : Application
     {
 
-      
+
         private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             System.Diagnostics.Debug.WriteLine(e.Exception.Message);
             MessageBox.Show("Ooooops... Something went wrong", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-         }
+        }
 
         private void Application_Exit(object sender, ExitEventArgs e)
         {
@@ -28,7 +28,7 @@ namespace MeasurementStudio
 
         private void Application_Startup(object sender, StartupEventArgs e)
         {
-           
+
         }
 
         private void Application_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
